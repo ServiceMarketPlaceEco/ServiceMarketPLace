c1e1df34319f0bbaaf59ee97cf46e1598727d629
tuoggg1 <104526513@student.swin.edu.au>
2026-09-11 15:14:52 +1000

    Add provider backend

--- backend/dist/common/decorators/current-user.decorator.d.ts
+++
@@ -1,7 +0,0 @@
-export interface CurrentUserData {
-    userId: string;
-    email: string;
-    userType: 'customer' | 'provider' | 'admin';
-    user: any;
-}
-export declare const CurrentUser: any;
